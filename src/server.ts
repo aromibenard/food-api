d52735fac7292409f26f05ca222388cb0dbc4680
Benard Aromi <112564277+aromibenard@users.noreply.github.com>
2025-05-18 12:40:44 +0000

    fix(package): update main entry point to server.js and adjust start script fix(server): add error handling for uncaught exceptions and unhandled rejections

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import app from "./app";
 
 const PORT = process.env.PORT || 3000;
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
